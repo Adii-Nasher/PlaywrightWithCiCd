604e7a45bd1cd7f4babce4999f0e2699b9ac0400
Gabriel <gabrielpebarbosa@gmail.com>
2024-12-16 14:13:13 +0000

    Adding logout automation

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -10,58 +10,72 @@ test.describe('Login Suite', () => {
     await expect(page.locator('.title')).toHaveText('Products');
   });
 
-test('Login with locked-out user', async ({ page }) => {
-  await page.goto('/');
-  await page.fill('#user-name', 'locked_out_user');
-  await page.fill('#password', 'secret_sauce');
-  await page.click('#login-button');
-  const errorMessage = await page.locator('[data-test="error"]').textContent();
-  expect(errorMessage).toBe('Epic sadface: Sorry, this user has been locked out.');
-});
+  test('Logout', async ({ page }) => {
+    await page.goto('/');
+    await page.fill('#user-name', 'standard_user');
+    await page.fill('#password', 'secret_sauce');
+    await page.click('#login-button');
 
-test('Successful login with a problem user', async ({ page }) => {
-  await page.goto('/');
-  await page.fill('#user-name', 'problem_user');
-  await page.fill('#password', 'secret_sauce');
-  await page.click('#login-button');
-  await expect(page).toHaveURL('/inventory.html');
-  const brokenImages = await page.locator('img').evaluateAll(images => images.filter(img => !img.complete || img.naturalHeight === 0));
-  expect(brokenImages.length).toBeGreaterThan(0);
-});
+    await page.click('#react-burger-menu-btn');
+    await page.click('#logout_sidebar_link');
 
-test('Successful login with a performance glitch user', async ({ page }) => {
-  await page.goto('/');
-  await page.fill('#user-name', 'performance_glitch_user');
-  await page.fill('#password', 'secret_sauce');
-  await page.click('#login-button');
-  const navigationTiming = await page.evaluate(() => performance.timing.loadEventEnd - performance.timing.navigationStart);
-  await expect(page).toHaveURL('/inventory.html');
-});
+    await expect(page).toHaveURL('/');
+    const loginButton = await page.locator('#login-button');
+    await expect(loginButton).toBeVisible();
+  });
 
-test('Login without a username', async ({ page }) => {
-  await page.goto('/');
-  await page.fill('#password', 'secret_sauce');
-  await page.click('#login-button');
-  const errorMessage = await page.locator('[data-test="error"]').textContent();
-  expect(errorMessage).toBe('Epic sadface: Username is required');
-});
+  test('Login with locked-out user', async ({ page }) => {
+    await page.goto('/');
+    await page.fill('#user-name', 'locked_out_user');
+    await page.fill('#password', 'secret_sauce');
+    await page.click('#login-button');
+    const errorMessage = await page.locator('[data-test="error"]').textContent();
+    expect(errorMessage).toBe('Epic sadface: Sorry, this user has been locked out.');
+  });
 
-test('Login without a password', async ({ page }) => {
-  await page.goto('/');
-  await page.fill('#user-name', 'standard_user');
-  await page.click('#login-button');
-  const errorMessage = await page.locator('[data-test="error"]').textContent();
-  expect(errorMessage).toBe('Epic sadface: Password is required');
-});
+  test('Successful login with a problem user', async ({ page }) => {
+    await page.goto('/');
+    await page.fill('#user-name', 'problem_user');
+    await page.fill('#password', 'secret_sauce');
+    await page.click('#login-button');
+    await expect(page).toHaveURL('/inventory.html');
+    const brokenImages = await page.locator('img').evaluateAll(images => images.filter(img => !img.complete || img.naturalHeight === 0));
+    expect(brokenImages.length).toBeGreaterThan(0);
+  });
 
-test('Login with Invalid Credentials', {
-  tag: '@smoke',
-}, async ({ page }) => { 
-  await page.goto('/');
-  await page.fill('#user-name', 'invalid_user');
-  await page.fill('#password', 'wrong_password');
-  await page.click('#login-button');
-  const errorMessage = await page.locator('[data-test="error"]').textContent();
-  expect(errorMessage).toBe('Epic sadface: Username and password do not match any user in this service');
-});
+  test('Successful login with a performance glitch user', async ({ page }) => {
+    await page.goto('/');
+    await page.fill('#user-name', 'performance_glitch_user');
+    await page.fill('#password', 'secret_sauce');
+    await page.click('#login-button');
+    const navigationTiming = await page.evaluate(() => performance.timing.loadEventEnd - performance.timing.navigationStart);
+    await expect(page).toHaveURL('/inventory.html');
+  });
+
+  test('Login without a username', async ({ page }) => {
+    await page.goto('/');
+    await page.fill('#password', 'secret_sauce');
+    await page.click('#login-button');
+    const errorMessage = await page.locator('[data-test="error"]').textContent();
+    expect(errorMessage).toBe('Epic sadface: Username is required');
+  });
+
+  test('Login without a password', async ({ page }) => {
+    await page.goto('/');
+    await page.fill('#user-name', 'standard_user');
+    await page.click('#login-button');
+    const errorMessage = await page.locator('[data-test="error"]').textContent();
+    expect(errorMessage).toBe('Epic sadface: Password is required');
+  });
+
+  test('Login with Invalid Credentials', {
+    tag: '@smoke',
+  }, async ({ page }) => { 
+    await page.goto('/');
+    await page.fill('#user-name', 'invalid_user');
+    await page.fill('#password', 'wrong_password');
+    await page.click('#login-button');
+    const errorMessage = await page.locator('[data-test="error"]').textContent();
+    expect(errorMessage).toBe('Epic sadface: Username and password do not match any user in this service');
+  });
 });
